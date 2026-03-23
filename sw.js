faa9b8c7a2f0adb7f414798c19167bcf35131e70
kkate2023 <147892308+kkate2023@users.noreply.github.com>
2026-03-23 21:17:52 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "luna-cycle-v1";
+const ASSETS = [
+  "/luna-cycle/",
+  "/luna-cycle/index.html",
+  "/luna-cycle/manifest.json"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
